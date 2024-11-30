cb1c7c6b7e29969c641671b42cf44fbb8943a223
saad <chsaadahmed93@gmail.com>
2024-11-30 14:42:33 -0700

    add: genre

--- 01.locallibrary/controllers/genreController.js
+++ 01.locallibrary/controllers/genreController.js
@@ -14,37 +14,40 @@ const getGenresList = async function (req, res, next) {
     }
 }
 
-const createGenreForm = async function (req, res, next) {
+const getGenreForm = async function (req, res, next) {
     try {
-        res.send('Not Implemented: Create genre Form');
+        res.render('genre_form', { title: 'Add Genres' })
     } catch (error) {
         return next(error);
     }
 }
 
-const createGenre = async function (req, res, next) {
-    try {
-        body("name")
-            .trim()
-            .isLength({ min: 3 })
-            .escape()
-            .withMessage('Genre must be specified');
-        const errors = validationResult(req);
-        const genre = new Genre({
-            name: req.body.name,
-        });
-        if (!errors.isEmpty()) {
-            res.render("genre_form", { title: "Create genre", genre: genre, errors: errors.array() });
-            return;
+const createGenre = [
+    body("genres")
+        .trim()
+        .isLength({ min: 3 })
+        .escape()
+        .withMessage('Genre must be specified'),
+    async function (req, res, next) {
+        try {
+            const errors = validationResult(req);
+            const genre = new Genre({
+                name: req.body.genres,
+            });
+            if (!errors.isEmpty()) {
+                res.render("genre_form", { title: "Create genre", genre: genre, errors: errors.array() });
+                return;
+            }
+            else {
+                await genre.save();
+                res.redirect(`/catalog/genres`);
+                console.log(`Genre is saved successfully`);
+            }
+        } catch (error) {
+            return next(error);
         }
-        else {
-            await genre.save();
-            console.log(`Genre is saved successfully`);
-        }
-    } catch (error) {
-        return next(error);
     }
-}
+]
 
 const updateGenre = async function (req, res, next) {
     try {
@@ -63,4 +66,4 @@ const deleteGenre = async function (req, res, next) {
 }
 
 
-export { getGenresList, createGenre, createGenreForm, updateGenre, deleteGenre };
+export { getGenresList, createGenre, getGenreForm, updateGenre, deleteGenre };
--- 01.locallibrary/routes/catalogRouter.js
+++ 01.locallibrary/routes/catalogRouter.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { createAuthorForm, getAuthorsList, createAuthor, getAuthorDetails, updateAuthor, getUpdateAuthorForm, deleteAuthor } from "../controllers/authorController.js";
-import { getGenresList } from "../controllers/genreController.js";
+import { createGenre, getGenreForm, getGenresList } from "../controllers/genreController.js";
 import { getCreateBookForm, createBook, getBooksList, getBookDetails, getUpdateBookForm, updateBook, deleteBook } from "../controllers/bookController.js";
 
 const router = Router();
@@ -20,6 +20,8 @@ router
     .get('/book/:id/update', getUpdateBookForm)
     .post('/book/:id/update', updateBook)
     .post('/book/:id/delete', deleteBook)
+    .get('/genre/create', getGenreForm)
+    .post('/genre/create', createGenre)
     .get('/genres', getGenresList);
 
 export default router;
